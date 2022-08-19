646160fe5c2015236957129bbf3ab1d1dbd96913
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-08-19 12:44:59 -0500

    hello world complete

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.print("Hello World! ");
+        System.out.print("My name is Sean. ");
+        System.out.println("I am learning Java.");
+        System.out.println(3 * 3);
+    }
+}
